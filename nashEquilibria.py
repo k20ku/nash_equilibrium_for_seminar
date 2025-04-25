8d508efcc5ef5866a32357acb729054556ecf4bc
k20ku <heizhaokuan@gmail.com>
2025-04-25 14:37:37 +0900

    Update nashEquilibria.py
    nashEquilibriaのコメント修正

--- nashEquilibria.py
+++ nashEquilibria.py
@@ -1,13 +1,20 @@
 from __future__ import annotations
 import sympy as sp
-
+"""
+主に使用するライブラリです（もう先輩は卒業してしまって開発する意味はないのですが．．．）
+Pythonに慣れるためであったり，便利だけど割とレガシーなSympyライブラリの便利な一部分を切り取って，モダンなPythonで仕上げていくことが目的です．
+"""
 
 # 利潤の計算式を保持するクラス
-# 順次newConditionsに置き換えていく予定
+# 順次salesStrategyに置き換えていく予定
 class Profit:
     profit = {'off':lambda P,Q,c: Q*(P-2*c),
                 'hy':lambda P,Q,c: Q*(P-c),
                 'on':lambda P,Q,c: Q*(P)}
+    """
+    Javaのstaticコンストラクタを模したものだったが，pythonはインスタンス化が容易であるためいらない．
+    削除予定
+    """
     @staticmethod
     def of(strategyName :str):
         return Profit.profit[strategyName]
@@ -16,7 +23,11 @@ class Profit:
     
 def oneminus(x):
     1-x
-
+"""
+実際に利用してもらうクラス．
+`game.py` を直接利用しないで，卒論の主な検証作業はこれでできるようにする．
+newConditionsが卒論の章番号に対応しているので，そこから，簡単にこのクラスを利用できる関数も追加したい．
+"""
 class MarketData:
     
     # 価格
@@ -27,14 +38,18 @@ class MarketData:
 
     firms :dict[str,int]={'a':0,'b':1}
 
-
+    """
+    使うときに必要最低限の引数だけでインスタンス化ができるように当時作ったが，改修していくにつれて場当たり的な感じが否めない．
+    データコンテナ化して，もっと均衡計算ロジックとデータ格納を分離していきたい．だいたい，メソッドにすればいいところが
+    フィールドになっていてダサい．
+    """
     def __init__(self,
                  quantity_a,
                  strategyName_a: str,
                  strategyName_b: str
                 ):
         self.Qa = quantity_a
-        self.Qb = lambda pa,pb: oneminus(self.Qa(pa,pb))
+        self.Qb = lambda pa,pb: oneminus(self.Qa(pa,pb)) # 実行時に
         self.Strgy_a, self.Strgy_b= strategyName_a, strategyName_b
         self.pi_a, self.pi_b = Profit.of(self.Strgy_a), Profit.of(self.Strgy_b)
 
